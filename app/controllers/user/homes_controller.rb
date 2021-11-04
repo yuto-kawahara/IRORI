7e78c5f5ef8193a77332fba1fcaf739d23a500c9
yuto-kawahara <mashroom1384@gmail.com>
2021-11-04 06:35:55 +0000

    [Add]HomesControllerのアクション実装

--- app/controllers/user/homes_controller.rb
+++ app/controllers/user/homes_controller.rb
@@ -1,2 +1,19 @@
 class User::HomesController < ApplicationController
+  def top
+  end
+
+  def home
+    @recruits = Recruit.following_user_recruit(current_user).sorted
+    @recruits = @recruits.includes(:user).page(params[:page])
+  end
+
+  def setting
+  end
+
+  def help
+  end
+
+  def contact
+  end
+
 end
