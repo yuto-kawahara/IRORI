0451a1c75bb590da43ef778d94972a6ba465ea8b
yuto-kawahara <mashroom1384@gmail.com>
2021-11-04 06:38:17 +0000

    [Add]RelationshipsControllerのアクション実装

--- app/controllers/user/relationships_controller.rb
+++ app/controllers/user/relationships_controller.rb
@@ -1,2 +1,17 @@
 class User::RelationshipsController < ApplicationController
+  before_action :set_user
+  include NotificationsHelper
+
+  def create
+    current_user.follow(@user)
+    create_notification(current_user, @user.id, nil, nil, nil, "follow")
+  end
+
+  def destroy
+    current_user.unfollow(@user)
+  end
+
+  def set_user
+    @user = User.find(params[:user_nickname])
+  end
 end
