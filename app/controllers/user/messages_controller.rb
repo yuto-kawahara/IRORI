88c780a379895f4d01996857c1184e1e00a7697e
yuto-kawahara <mashroom1384@gmail.com>
2021-11-04 06:42:06 +0000

    [Add]MessagesControllerのアクション実装

--- app/controllers/user/messages_controller.rb
+++ app/controllers/user/messages_controller.rb
@@ -1,2 +1,29 @@
 class User::MessagesController < ApplicationController
+  include MessagesHelper
+  include NotificationsHelper
+
+  def index
+    @rooms = current_user.rooms
+    @user_rooms = UserRoom.where(room_id: @rooms).sorted
+    @user_rooms = @user_rooms.where.not(user_id: current_user.id)
+  end
+
+  def create
+    @message = current_user.messages.new(message_params)
+    @message.save
+    users = @message.room.users
+    user_id = users.where.not(id: current_user.id).first.id
+    create_notification(current_user, user_id, nil, nil, @message.id, "message")
+  end
+
+  def show
+    @user = User.find(params[:id])
+    room_create_search(current_user, @user.id, "", "one")
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content, :room_id)
+  end
 end
