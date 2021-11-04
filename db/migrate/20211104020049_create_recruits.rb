be001527cbc81449d38b1da3efa74c3aed496036
yuto-kawahara <mashroom1384@gmail.com>
2021-11-04 10:58:21 +0000

    [Add]エラー対応中

--- db/migrate/20211104020049_create_recruits.rb
+++ db/migrate/20211104020049_create_recruits.rb
@@ -5,10 +5,10 @@ class CreateRecruits < ActiveRecord::Migration[5.2]
       t.string     :title,                 null: false, default: ""
       t.string     :image_id,              null: false, default: ""
       t.datetime   :start_time,            null: false
-      t.integer    :time_required,         null: false, default: ""
-      t.integer    :capacity,              null: false, default: ""
+      t.integer    :time_required,         null: false
+      t.integer    :capacity,              null: false
       t.text       :explanation,           null: false
-      t.string     :discord_server_link,   null: false, default: ""
+      t.string     :discord_server_link,   null: false
       t.integer    :recruit_status,        null: false, default: 1
       t.integer    :recruit_comments_count,null: false, default: 0
       t.integer    :reserves_count,        null: false, default: 0
