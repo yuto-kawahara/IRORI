f78df3c750c6a142fc580cfbf45cee392767f2b7
yuto-kawahara <mashroom1384@gmail.com>
2021-11-04 06:40:43 +0000

    [Add]NotificationsControllerのアクション実装

--- app/controllers/user/notifications_controller.rb
+++ app/controllers/user/notifications_controller.rb
@@ -1,2 +1,10 @@
 class User::NotificationsController < ApplicationController
+  def index
+    @notifications = current_user.passive_notifications.sorted
+    @notifications = @notifications.includes(:recruit, :recruit_comment, :message, :visitor)
+    @notifications = @notifications.page(params[:page])
+    @notifications.unread.each do |notification|
+      notification.update_attribute(:checked, true)
+    end
+  end
 end
