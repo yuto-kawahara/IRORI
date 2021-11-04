01e39dd484d3ddad2ff3c368014a46c74e470957
yuto-kawahara <mashroom1384@gmail.com>
2021-11-04 12:37:37 +0000

    [Fix]マイグレーションファイル修正

--- db/migrate/20211104020152_create_messages.rb
+++ db/migrate/20211104020152_create_messages.rb
@@ -2,7 +2,7 @@ class CreateMessages < ActiveRecord::Migration[5.2]
   def change
     create_table :messages do |t|
       t.integer :user_id,   null: false
-      t.integer :room_id,   null: false, default: ""
+      t.integer :room_id,   null: false
       t.text    :content,   null: false
       t.timestamps
     end
