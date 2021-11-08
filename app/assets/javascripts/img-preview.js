bde8fe36ceb184ed5828eecaee2087b19d3b0b80
yuto-kawahara <mashroom1384@gmail.com>
2021-11-08 14:44:34 +0000

    [Fix]img-preview.jsを修正

--- app/assets/javascripts/img-preview.js
+++ app/assets/javascripts/img-preview.js
@@ -6,14 +6,7 @@ $(document).on('turbolinks:load', function() {
     var fileReader = new FileReader();
     fileReader.onloadend = function() {
       var src = fileReader.result
-      var html= `<div class='item_img' data-image="${file.name}">
-                   <div class='item_img__inner'>
-                     <img src="${src}" class="upload_img">
-                     <button type='button' class='btn btn__img_delete'>
-                       <i class="fas fa-times"></i>
-                     </button>
-                   </div>
-                 </div>`
+      var html= "<div class='item_img' data-image='${file.name}'><div class='item_img__inner'><img src='${src}' class='upload_img'><button type='button' class='btn btn__img_delete'><i class='fas fa-times'></i></button></div></div>"
       $('.img_wrapper').before(html);
     }
     fileReader.readAsDataURL(file);
