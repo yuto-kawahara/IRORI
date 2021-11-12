f701276636244947a4447e63c4d452a26b9a1247
yuto-kawahara <mashroom1384@gmail.com>
2021-11-12 07:28:29 +0000

    [Add]レスポンシブ対応

--- app/assets/javascripts/address-location.js
+++ app/assets/javascripts/address-location.js
@@ -10,4 +10,15 @@ $(document).on('turbolinks:load', function () {
       $(this).removeClass('active');
     }
   });
+
+  $('.footer_menu__link').each(function() {
+    var $href = $(this).attr('href');
+
+    if (location.pathname == $href) {
+      $(this).addClass('active');
+    }
+    else {
+      $(this).removeClass('active');
+    }
+  });
 });
