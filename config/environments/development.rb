beb36b5ab9f5fc57586b2cbd2599092b87437005
yuto-kawahara <mashroom1384@gmail.com>
2021-11-13 01:22:20 +0000

    [Fix]機密情報の保持方法を変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,8 +37,8 @@ Rails.application.configure do
     port:                 587,
     address:              'smtp.gmail.com',
     domain:               'gmail.com',
-    user_name:            Rails.application.credentials[:admin][:email],
-    password:             Rails.application.credentials[:admin][:password],
+    user_name:            ENV['ADMIN_EMAIL'],
+    password:             ENV['ADMIN_PASSWORD'],
     authentication:       'login',
     enable_starttls_auto: true
   }
