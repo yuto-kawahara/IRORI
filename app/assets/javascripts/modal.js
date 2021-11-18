9605a6f4405faeba67b18c98eead36315bbbec92
yuto-kawahara <mashroom1384@gmail.com>
2021-11-18 08:45:15 +0000

    [Fix]無限スクロール時に削除できないバグを修正

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,19 +1,36 @@
 /*global $*/
 $(document).on('turbolinks:load', function () {
+  var target = $('.list');
+  const config = { attributes: false, childList: true, subtree: true };
   var cover = $('#cover');
   var ellipsis_h = $('.ellipsis_h');
-  var ellipsis_v = $('.ellipsis_v');
+
 
   ellipsis_h.click(function() {
     cover.addClass("hidden");
     $('.recruit_change_area').toggleClass("open");
   });
 
-  ellipsis_v.click(function() {
+  var ellipsis_v = $('.ellipsis_v');
+
+  target.on('click', '.ellipsis_v', function() {
     cover.addClass("hidden");
     $(this).find('.option_modal').toggleClass("open");
   });
 
+  // // modal_v();
+  // if (target !== null) {
+  //   var observer = new MutationObserver(function(record) {
+  //     observer.disconnect();
+  //     var ellipsis_v = $('.ellipsis_v');
+  //     ellipsis_v.click(function() {
+  //       cover.addClass("hidden");
+  //       $(this).find('.option_modal').toggleClass("open");
+  //     });
+  //     observer.observe(target, config);
+  //   });
+  //   observer.observe(target, config);
+  // }
 
   cover.click(function() {
     cover.removeClass("hidden");
