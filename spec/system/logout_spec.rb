7e18af7ef1693e8c50530b9bd72389012554252b
yuto-kawahara <mashroom1384@gmail.com>
2021-11-25 00:03:25 +0000

    [Add]テストデータ追加

--- spec/system/logout_spec.rb
+++ spec/system/logout_spec.rb
@@ -9,15 +9,12 @@ describe 'ユーザログアウトのテスト' do
     fill_in 'user[password]', with: user.password
     click_button 'ログイン'
     visit setting_path
-    logout_link = find_all('a')[12].native.inner_text
+    logout_link = find_all('a')[13].native.inner_text
     logout_link = logout_link.gsub(/\n/, '').gsub(/\A\s*/, '').gsub(/\s*\Z/, '')
     click_link logout_link
   end
 
   context 'ログアウト機能のテスト' do
-    it '正しくログアウトできている: ログアウト後のリダイレクト先において新規登録画面へのリンクが存在する' do
-      expect(page).to have_link '', href: new_user_registration_path
-    end
     it 'ログアウト後のリダイレクト先が、トップになっている' do
       expect(current_path).to eq '/'
     end
--- spec/system/sidemenu_spec.rb
+++ spec/system/sidemenu_spec.rb
@@ -23,31 +23,31 @@ describe 'サイドメニューのテスト' do
       expect(side_menu_link).to match(/プロフィール/i)
     end
     it '募集リンクが表示される: 左上から4番目のリンクが「募集」である' do
-      side_menu_link = find_all('a')[3].native.inner_text
+      side_menu_link = find_all('a')[4].native.inner_text
       expect(side_menu_link).to match(/募集/i)
     end
     it '卓報告リンクが表示される: 左上から5番目のリンクが「卓報告」である' do
-      side_menu_link = find_all('a')[4].native.inner_text
+      side_menu_link = find_all('a')[5].native.inner_text
       expect(side_menu_link).to match(/卓報告/i)
     end
     it 'メッセージリンクが表示される: 左上から6番目のリンクが「メッセージ」である' do
-      side_menu_link = find_all('a')[5].native.inner_text
+      side_menu_link = find_all('a')[6].native.inner_text
       expect(side_menu_link).to match(/メッセージ/i)
     end
     it '通知リンクが表示される: 左上から7番目のリンクが「通知」である' do
-      side_menu_link = find_all('a')[6].native.inner_text
+      side_menu_link = find_all('a')[7].native.inner_text
       expect(side_menu_link).to match(/通知/i)
     end
     it 'スケジュールリンクが表示される: 左上から8番目のリンクが「スケジュール」である' do
-      side_menu_link = find_all('a')[7].native.inner_text
+      side_menu_link = find_all('a')[8].native.inner_text
       expect(side_menu_link).to match(/スケジュール/i)
     end
     it '設定リンクが表示される: 左上から9番目のリンクが「設定」である' do
-      side_menu_link = find_all('a')[8].native.inner_text
+      side_menu_link = find_all('a')[9].native.inner_text
       expect(side_menu_link).to match(/設定/i)
     end
     it '募集するリンクが表示される: 左上から10番目のリンクが「募集する」である' do
-      side_menu_link = find_all('a')[9].native.inner_text
+      side_menu_link = find_all('a')[10].native.inner_text
       expect(side_menu_link).to match(/募集する/i)
     end
   end
@@ -72,37 +72,37 @@ describe 'サイドメニューのテスト' do
       is_expected.to eq '/users/' + user.nickname
     end
     it '募集を押すと、募集画面に遷移する' do
-      recruit_link = find_all('.side_menu__link')[2].native.inner_text
+      recruit_link = find_all('.side_menu__link')[3].native.inner_text
       recruit_link = recruit_link.gsub(/\n/, '').gsub(/\A\s*/, '').gsub(/\s*\Z/, '')
       click_link recruit_link, match: :first
-      is_expected.to eq '/recruits'
+      is_expected.to eq '/recruits/schedule/today'
     end
     it '卓報告を押すと、卓報告画面に遷移する' do
-      evaluation_link = find_all('.side_menu__link')[3].native.inner_text
+      evaluation_link = find_all('.side_menu__link')[4].native.inner_text
       evaluation_link = evaluation_link.gsub(/\n/, '').gsub(/\A\s*/, '').gsub(/\s*\Z/, '')
       click_link evaluation_link
       is_expected.to eq '/users/' + user.nickname + '/evaluations'
     end
     it 'メッセージを押すと、メッセージ画面に遷移する' do
-      message_link = find_all('.side_menu__link')[4].native.inner_text
+      message_link = find_all('.side_menu__link')[5].native.inner_text
       message_link = message_link.gsub(/\n/, '').gsub(/\A\s*/, '').gsub(/\s*\Z/, '')
       click_link message_link
       is_expected.to eq '/messages'
     end
     it '通知を押すと、通知画面に遷移する' do
-      notice_link = find_all('.side_menu__link')[5].native.inner_text
+      notice_link = find_all('.side_menu__link')[6].native.inner_text
       notice_link = notice_link.gsub(/\n/, '').gsub(/\A\s*/, '').gsub(/\s*\Z/, '')
       click_link notice_link, match: :first
       is_expected.to eq '/notifications'
     end
     it 'スケジュールを押すと、スケジュール画面に遷移する' do
-      schedule_link = find_all('.side_menu__link')[6].native.inner_text
+      schedule_link = find_all('.side_menu__link')[7].native.inner_text
       schedule_link = schedule_link.gsub(/\n/, '').gsub(/\A\s*/, '').gsub(/\s*\Z/, '')
       click_link schedule_link
       is_expected.to eq '/users/' + user.nickname + '/schedule'
     end
     it '設定を押すと、設定画面に遷移する' do
-      setting_link = find_all('.side_menu__link')[7].native.inner_text
+      setting_link = find_all('.side_menu__link')[8].native.inner_text
       setting_link = setting_link.gsub(/\n/, '').gsub(/\A\s*/, '').gsub(/\s*\Z/, '')
       click_link setting_link
       is_expected.to eq '/setting'
